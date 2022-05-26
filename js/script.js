96810476cb879418a7ed187423472d6717b73409
Kardast <dottorsf@gmail.com>
2022-05-26 16:40:05 +0200

    Add and remove tasks

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const myList = new Vue ({
     el: "#my_space",
     // i miei dati
     data: {
-
+        newTodo: "",
         todos : [
             {
                 text: "Aprire il recinto alle papere",
@@ -25,12 +25,24 @@ const myList = new Vue ({
             {
                 text: "Iniziare a programmare",
                 done: false
-            }
+            },
         ]
     },
     // i miei metodi
     methods : {
 
+        addItem(){
 
+            const task = new Object();
+            task.text = this.newTodo;
+            task.done = false;
+            if (this.newTodo !== "") {
+                this.todos.push(task);
+                this.newTodo = "";
+            }
+        },
+        deleteItem(index){
+            this.todos.splice(index,1);
+        }
     }
 }); 
